e052cbeedf50ff1b4d911795dfaa292d87a40a47
Marvey Gold <145897274+MarveyGold@users.noreply.github.com>
2026-06-07 23:32:30 +0100

    Add files via upload

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+  import "../../app.css";
+
+  import MobileNav from "$lib/components/MobileNav.svelte";
+  import Header from "$lib/components/Header.svelte";
+
+  let { children } = $props();
+</script>
+
+<Header />
+<main>
+  {@render children()}
+</main>
+
+<MobileNav />
