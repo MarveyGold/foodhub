6ffdfc0dae253df44f2bc8aef4d860964bf4417f
Marvey Gold <145897274+MarveyGold@users.noreply.github.com>
2026-06-11 15:29:41 +0100

    Delete

--- src/routes/(app)/menu/delete/+page.svelte
+++ src/routes/(app)/menu/delete/+page.svelte
@@ -1,1 +1,13 @@
+<script>
+  import { enhance } from '$app/forms';
+  let { data } = $props();
+</script>
 
+{#each data.menu as product}
+  <p>{product.name}</p>
+
+  <form method="POST" action="?/delete" use:enhance>
+    <input type="hidden" name="_id" value={product._id} />
+    <button type="submit">Delete</button>
+  </form>
+{/each}
