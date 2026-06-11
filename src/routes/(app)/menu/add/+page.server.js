b0cf93972f66d6c956e3ad94b77ab5891cf1f78e
Marvey Gold <145897274+MarveyGold@users.noreply.github.com>
2026-06-11 14:45:50 +0100

    Update image upload to Cloudinary service
    Refactor image upload to use Cloudinary API instead of local file system.

--- src/routes/(app)/menu/add/+page.server.js
+++ src/routes/(app)/menu/add/+page.server.js
@@ -1,5 +1,4 @@
-import { writeFile } from 'fs/promises';
-import path from 'path';
+import { CLOUDINARY_CLOUD_NAME, CLOUDINARY_UPLOAD_PRESET } from '$env/static/private';
 
 export const actions = {
   default: async ({ request }) => {
@@ -8,18 +7,18 @@ export const actions = {
     const name = data.get('name');
     const image = data.get('image');
 
-    const buffer = Buffer.from(
-      await image.arrayBuffer()
-    );
-    console.log(name);
-    console.log(image.name);
-    await writeFile(
-      path.join('static', `${name}.png`),
-      buffer
+    const formData = new FormData();
+    formData.append('file', image);
+    formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+    formData.append('public_id', name);
+
+    const res = await fetch(
+      `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`,
+      { method: 'POST', body: formData }
     );
 
-    return {
-      success: true
-    };
+    const result = await res.json();
+
+    return { success: true, url: result.secure_url };
   }
 };
