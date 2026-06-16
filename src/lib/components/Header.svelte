79af406194a3cffe85d27280a5534415544dd5a0
Marvey Gold <145897274+MarveyGold@users.noreply.github.com>
2026-06-16 01:21:11 +0100

    m [skip pxxl]

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,5 @@
-<a href="/">
-  <header class="backdrop-blur-sm bg-black-0">
+<header class="backdrop-blur-sm bg-black-0">
+  <a href="/">
     <div class="header-left">
       <button class="icon-btn" aria-label="Go back">
         <span class="material-symbols-outlined primary-icon"
@@ -13,24 +13,24 @@
       </button>
       <h1 class="brand">Hadeva Bakes</h1>
     </div>
-    <div class="header-right">
-      <span class=" icon-btn"
-        ><svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          fill="currentColor"
-          viewBox="0 0 24 24"
-        >
-          <!--Boxicons v3.0.8 https://boxicons.com | License  https://docs.boxicons.com/free-->
-          <path
-            d="M12 12c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5m0-8c1.65 0 3 1.35 3 3s-1.35 3-3 3-3-1.35-3-3 1.35-3 3-3M4 22h16c.55 0 1-.45 1-1v-1c0-3.86-3.14-7-7-7h-4c-3.86 0-7 3.14-7 7v1c0 .55.45 1 1 1m6-7h4c2.76 0 5 2.24 5 5H5c0-2.76 2.24-5 5-5"
-          ></path>
-        </svg></span
+  </a>
+  <div class="header-right">
+    <span class=" icon-btn"
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="currentColor"
+        viewBox="0 0 24 24"
       >
-    </div>
-  </header>
-</a>
+        <!--Boxicons v3.0.8 https://boxicons.com | License  https://docs.boxicons.com/free-->
+        <path
+          d="M12 12c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5m0-8c1.65 0 3 1.35 3 3s-1.35 3-3 3-3-1.35-3-3 1.35-3 3-3M4 22h16c.55 0 1-.45 1-1v-1c0-3.86-3.14-7-7-7h-4c-3.86 0-7 3.14-7 7v1c0 .55.45 1 1 1m6-7h4c2.76 0 5 2.24 5 5H5c0-2.76 2.24-5 5-5"
+        ></path>
+      </svg></span
+    >
+  </div>
+</header>
 
 <style>
   header {
