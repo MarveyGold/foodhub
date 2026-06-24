b423e8a7aac86ee99cc5c2106305eebbb411aa29
Marvey <osoidaghemarvel@gmail.com>
2026-06-24 20:13:23 +0100

    remove

--- .svelte-kit/generated/client/nodes/5.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/(admin)/add-menu/+page.svelte";
--- .svelte-kit/generated/client/nodes/9.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/(app)/menu/+page.svelte";
